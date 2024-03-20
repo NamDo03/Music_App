581cbe3d0587e5b4022d038ae4430e905d0bc966
Admin <zaihole2003@gmail.com>
2024-03-20 21:11:33 +0700

    Album cua Ngoc

--- app/src/main/java/com/example/music_app/MainActivity.java
+++ app/src/main/java/com/example/music_app/MainActivity.java
@@ -1,14 +1,110 @@
 package com.example.music_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.music_app.adapter.CategoryAdapter;
+import com.example.music_app.adapter.SectionSongListAdapter;
+import com.example.music_app.databinding.ActivityMainBinding;
+import com.example.music_app.models.CategoryModel;
+import com.example.music_app.models.SongModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getCategories();
+        setupSection("section_1",binding.section1MainLayout,binding.section1Title,binding.section1RecyclerView);
+        setupSection("section_2",binding.section2MainLayout,binding.section2Title,binding.section2RecyclerView);
+        setupSection("section_3",binding.section3MainLayout,binding.section3Title,binding.section3RecyclerView);
+    }
+
+    public void setupCategoryRecyclerView(List<CategoryModel> categoryList) {
+        categoryAdapter = new CategoryAdapter(categoryList);
+        binding.categoriesRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        binding.categoriesRecyclerView.setAdapter(categoryAdapter);
+    }
+
+    public void getCategories() {
+        FirebaseFirestore.getInstance().collection("category")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<CategoryModel> categoryList = queryDocumentSnapshots.toObjects(CategoryModel.class);
+                        setupCategoryRecyclerView(categoryList);
+                    }
+                });
+    }
+    public void setupSection(String id, RelativeLayout mainLayout, TextView titleView, RecyclerView recyclerView) {
+        FirebaseFirestore.getInstance().collection("sections")
+                .document(id)
+                .get().addOnSuccessListener(documentSnapshot -> {
+                    CategoryModel section = documentSnapshot.toObject(CategoryModel.class);
+                    if (section != null) {
+                        mainLayout.setVisibility(View.VISIBLE);
+                        titleView.setText(section.getName());
+                        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false));
+                        recyclerView.setAdapter(new SectionSongListAdapter(section.getSongs()));
+                        mainLayout.setOnClickListener(view -> {
+                            SongsListActivity.setCategory(section);
+                            startActivity(new Intent(MainActivity.this, SongsListActivity.class));
+                        });
+                    }
+                });
+    }
+
+    public void setupMostlyPlayed(String id, RelativeLayout mainLayout, TextView titleView, RecyclerView recyclerView) {
+        FirebaseFirestore.getInstance().collection("sections")
+                .document(id)
+                .get().addOnSuccessListener(documentSnapshot -> {
+                    FirebaseFirestore.getInstance().collection("songs")
+                            .orderBy("count", Query.Direction.DESCENDING)
+                            .limit(5)
+                            .get().addOnSuccessListener(songListSnapshot -> {
+                                List<SongModel> songsModelList = songListSnapshot.toObjects(SongModel.class);
+                                List<String> songsIdList = new ArrayList<>();
+                                for (SongModel songModel : songsModelList) {
+                                    songsIdList.add(songModel.getId());
+                                }
+                                CategoryModel section = documentSnapshot.toObject(CategoryModel.class);
+                                if (section != null) {
+                                    section.setSongs(songsIdList);
+                                    mainLayout.setVisibility(View.VISIBLE);
+                                    titleView.setText(section.getName());
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false));
+                                    recyclerView.setAdapter(new SectionSongListAdapter(section.getSongs()));
+                                    mainLayout.setOnClickListener(view -> {
+                                        SongsListActivity.setCategory(section);
+                                        startActivity(new Intent(MainActivity.this, SongsListActivity.class));
+                                    });
+                                }
+                            });
+                });
     }
 }
